455400dcd73823e94b348469dace77ab9f1e9d9e
Peter Phyall <pdrogos02@gmail.com>
2024-02-29 18:51:25 -0600

    /feature app file cleanup, output cleanup

--- crisp_app.py
+++ crisp_app.py
@@ -1,26 +1,19 @@
-import io, yaml, os, shutil
-
-import pandas as pd
-from contextlib import redirect_stderr
-from decimal import Decimal
+import os, shutil
 
 from flask import Flask, render_template, request, flash, redirect, url_for
 from werkzeug.utils import secure_filename
 
-from utils import get_logger, create_new_col, allowed_file
+from utils import allowed_file
+from crisp_transformation import perform_transformation
 
-# from crisp_transformation import execute_transformation
-
-# app = Flask(__name__, template_folder='templates')
-
-app = Flask(__name__, static_url_path='/static', template_folder='templates')  
+# app = Flask(__name__, static_url_path='/static', template_folder='templates')  
+app = Flask(__name__, template_folder='templates')  
 
 app.config.from_object('flask_config')
 
 #TODO add in exception handling for two API endpoints
 
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
     return render_template('index.html')
 
@@ -65,12 +58,9 @@ def transform():
         if not os.path.exists(app.config['UPLOAD_FOLDER']):
             os.makedirs(app.config['UPLOAD_FOLDER'])
 
-
         crisp_config_yaml_filename = secure_filename(crisp_config_yaml_file.filename)
 
         crisp_config_yaml_file.save(os.path.join(app.config['UPLOAD_FOLDER'], crisp_config_yaml_filename))
-
-       
 
         input_data_filename = secure_filename(input_data_file.filename)
 
@@ -87,82 +77,12 @@ def transform():
 
         #TODO need to modularize below code 
         # beginning of transformation code
-        try:
-            logger = get_logger()
+        raw_df_shape, transformed_df = perform_transformation()
 
-        except Exception as e:
-            raise e
-    
-        try:
-            with open(os.path.join(app.config['UPLOAD_FOLDER'], crisp_config_yaml_filename), "r") as file:
-                config_dict = yaml.safe_load(file)
-
-        except Exception as e:
-            logger.error(f'Unable to read in config file: {e}')
-
-            raise e
-        
-        f = io.StringIO()
-
-        # can use file-like object in memory to avoid saving file
-        with redirect_stderr(f):
-            # raw_df = pd.read_csv(request.files.get('input_data_file'), on_bad_lines='warn', encoding='utf8')
-            raw_df = pd.read_csv(os.path.join(app.config['UPLOAD_FOLDER'], input_data_filename), on_bad_lines='warn')
-
-        if f.getvalue():
-            logger.warning(f"Reading input data lines - bad line(s): \n{f.getvalue()}")
-
-        try:
-            # transformation, step 1: rename cols
-            raw_df = raw_df.rename(columns=config_dict['renamed_cols'])
-
-            # transformation, step 2: create new cols
-            for key, value in config_dict['new_cols'].items():
-                raw_df = create_new_col(raw_df, key, value)
-
-            # transformation, step 3: convert col dtypes
-            for key, value in config_dict['dtype_cols'].items():
-                if 'int' in key or 'str' in key:
-                    raw_df[value] = raw_df[value].astype(key)
-                
-                elif 'datetime' in key:
-                    raw_df[value] = raw_df[value].apply(pd.to_datetime)
-
-                elif 'decimal' in key:
-                    raw_df[value] = raw_df[value].apply(lambda x: x.str.replace(',', "")).apply(lambda x: x.apply(Decimal))
-
-            # 4) transformation, step 4: manipulate str col dtypes
-            for key, value in config_dict['str_dtype_cols_manipulation'].items():
-                if 'proper_case' in key:
-                    raw_df[value] = raw_df[value].apply(lambda x: x.str.title())
-
-            # final df displayed as html
-            transformed_df = raw_df[['OrderId', 'OrderDate', 'ProductId', 'ProductName', 'Quantity', 'Unit']]
-
-        except Exception as e:
-            logger.error(f"Error in transforming data: {e}")
-
-            raise e
-        
-        # return render_template('success.html', crisp_config_yaml_file_name=crisp_config_yaml_filename, input_data_file_name=input_data_filename)
-        # return render_template('output.html',  name='Transformed Data', input_data_file_name= input_data_filename, crisp_config_yaml_file_name=crisp_config_yaml_filename, input_data_file_shape=raw_df.shape, data=transformed_df.to_html())
-    
-        return render_template('success.html')
-    
-        # return redirect(url_for('success'))
+        return render_template('output.html', input_data_file_name=input_data_filename, crisp_config_yaml_file_name=crisp_config_yaml_filename, input_data_file_shape=raw_df_shape, data=transformed_df.head(20).to_html(), transformed_data_shape=transformed_df.shape)
     
     else:
         return render_template('transform.html')
-    
-
-# @app.route('/transform2', methods=['GET', 'POST'])
-# def transform():
-#     if request.method == 'POST':
-#         pass
-
-#     else:
-#         return render_template('transform.html')
-
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
---
+++ crisp_transformation.py
@@ -0,0 +1,80 @@
+import io, yaml, os
+
+from contextlib import redirect_stderr
+from decimal import Decimal
+
+import pandas as pd
+
+from utils import get_logger, create_new_col
+
+from flask import current_app, request
+from werkzeug.utils import secure_filename
+
+
+def perform_transformation():
+    try:
+        logger = get_logger()
+
+    except Exception as e:
+        raise e
+
+    crisp_config_yaml_file = request.files['crisp_config_yaml_file']
+        
+    crisp_config_yaml_filename = secure_filename(crisp_config_yaml_file.filename)
+
+    input_data_file = request.files["input_data_file"]
+
+    input_data_filename = secure_filename(input_data_file.filename)
+
+    try:
+        with open(os.path.join(current_app.config['UPLOAD_FOLDER'], crisp_config_yaml_filename), "r") as file:
+            config_dict = yaml.safe_load(file)
+
+    except Exception as e:
+        logger.error(f'Unable to read in config file: {e}')
+
+        raise e
+    
+    f = io.StringIO()
+
+    # can use file-like object in memory to avoid saving file
+    with redirect_stderr(f):
+        # raw_df = pd.read_csv(request.files.get('input_data_file'), on_bad_lines='warn', encoding='utf8')
+        raw_df = pd.read_csv(os.path.join(current_app.config['UPLOAD_FOLDER'], input_data_filename), on_bad_lines='warn')
+
+    if f.getvalue():
+        logger.warning(f"Reading input data lines - bad line(s): \n{f.getvalue()}")
+
+    try:
+        # transformation, step 1: rename cols
+        raw_df = raw_df.rename(columns=config_dict['renamed_cols'])
+
+        # transformation, step 2: create new cols
+        for key, value in config_dict['new_cols'].items():
+            raw_df = create_new_col(raw_df, key, value)
+
+        # transformation, step 3: convert col dtypes
+        for key, value in config_dict['dtype_cols'].items():
+            if 'int' in key or 'str' in key:
+                raw_df[value] = raw_df[value].astype(key)
+            
+            elif 'datetime' in key:
+                raw_df[value] = raw_df[value].apply(pd.to_datetime)
+
+            elif 'decimal' in key:
+                raw_df[value] = raw_df[value].astype(str).apply(lambda x: x.str.replace(',', "")).apply(lambda x: x.apply(Decimal))
+
+        # 4) transformation, step 4: manipulate str col dtypes
+        for key, value in config_dict['str_dtype_cols_manipulation'].items():
+            if 'proper_case' in key:
+                raw_df[value] = raw_df[value].apply(lambda x: x.str.title())
+
+        # final df displayed as html
+        transformed_df = raw_df[['OrderId', 'OrderDate', 'ProductId', 'ProductName', 'Quantity', 'Unit']]
+
+    except Exception as e:
+        logger.error(f"Error in transforming data: {e}")
+
+        raise e
+    
+    return raw_df.shape, transformed_df
