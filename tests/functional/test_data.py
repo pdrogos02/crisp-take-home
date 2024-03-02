7964ebe4e9e77503f1949919b9caaf7d339e6d68
Peter Phyall <pdrogos02@gmail.com>
2024-03-02 16:32:36 -0600

    /feature data and file functional tests

--- tests/functional/test_data.py
+++ tests/functional/test_data.py
@@ -1,4 +1,19 @@
-import pytest
-from flask import session, g
+def test_data_transform_get(client):
+    files={'input_data_file': open('/Users/peterphyall/Documents/profdev/crisp-take-home/bad_lines_dummy_file_crisp.csv', 'rb'),
+            'crisp_config_yaml_file': open('/Users/peterphyall/Documents/profdev/crisp-take-home/crisp_config.yml', 'rb')}
 
+    upload_response = client.post('/file/upload', data=files)
 
+    response = client.get('/data/transform')
+
+    assert b"Confirm Uploaded Input Files and Transform" in response.data
+
+def test_data_transform(client):
+    files={'input_data_file': open('/Users/peterphyall/Documents/profdev/crisp-take-home/bad_lines_dummy_file_crisp.csv', 'rb'),
+            'crisp_config_yaml_file': open('/Users/peterphyall/Documents/profdev/crisp-take-home/crisp_config.yml', 'rb')}
+
+    upload_response = client.post('/file/upload', data=files)
+
+    response = client.post('/data/transform')
+    
+    assert b"Transformed Output - Crisp App" in response.data
--- tests/functional/test_file.py
+++ tests/functional/test_file.py
@@ -1,6 +1,17 @@
-import pytest
-from flask import session, g
-
-def test_index(client):
+def test_index_get(client):
     response = client.get('/')
+
     assert b"Welcome to the Crisp Transformation App" in response.data
+
+def test_file_upload_get(client):
+    response = client.get('/file/upload')
+
+    assert b"Upload Input Files" in response.data
+
+def test_file_upload(client):
+    files={'input_data_file': open('/Users/peterphyall/Documents/profdev/crisp-take-home/bad_lines_dummy_file_crisp.csv', 'rb'),
+            'crisp_config_yaml_file': open('/Users/peterphyall/Documents/profdev/crisp-take-home/crisp_config.yml', 'rb')}
+
+    response = client.post('/file/upload', data=files)
+    
+    assert b"Input File Upload Success" in response.data
