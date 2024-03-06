5ca588187e62c7b90e499e201965da7e135f7eec
Peter Phyall <pdrogos02@gmail.com>
2024-03-06 00:36:35 -0600

    /bugfix updating test input data file paths (from prior directory restructure)

--- tests/functional/test_data.py
+++ tests/functional/test_data.py
@@ -9,8 +9,8 @@ def test_data_transform_get(client):
     assert b"Confirm Uploaded Input Files and Transform" in response.data
 
 def test_data_transform(client):
-    files={'input_data_file': open('/Users/peterphyall/Documents/profdev/crisp-take-home/bad_lines_dummy_file_crisp.csv', 'rb'),
-            'crisp_config_yaml_file': open('/Users/peterphyall/Documents/profdev/crisp-take-home/crisp_config.yml', 'rb')}
+    files={'input_data_file': open('/Users/peterphyall/Documents/profdev/crisp-take-home/data/bad_lines_dummy_file_crisp.csv', 'rb'),
+            'crisp_config_yaml_file': open('/Users/peterphyall/Documents/profdev/crisp-take-home/data/crisp_config.yml', 'rb')}
 
     upload_response = client.post('/file/upload', data=files)
 
